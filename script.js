ba9964279a4e55cab1a91b12353aa06708eba15d
Prakash Shrestha <rzprakash16@gmail.com>
2024-07-05 07:09:23 +0545

    new-changes

---
+++ script.js
@@ -0,0 +1,30 @@
+const myFavFruits = ["apple", "mango", "banana"];
+const myFavMovies = ["abc", "cbd"]
+
+const cart = [
+    {
+        name: "bag",
+        price:29
+    },
+    {
+        name: "purse",
+        price:2
+    },
+    {
+        name: "belt",
+        price:9
+    }
+]
+myFavFruits.shift()
+//pop==> remove last element from array;
+//push==? add elemet at last;
+//unshift==> add elemt at first;
+//shift==?remove first  element;
+
+const sum = (a, b,c) => {
+    const result = a * b;
+    console.log
+    return result;
+};
+sum(1,3)
+console.log(sum(4,5,))
